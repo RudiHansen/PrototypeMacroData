94ad605f2e4573a46795761f5c6ea0753c46e3c7
RudiHansen <rsh@pobox.com>
2014-02-10 20:53:52 +0100

    Oprettelse af klasse til Data om macroer

---
+++ PrototypeMacroData/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeMacroData
+{
+    class MacroDataClass
+    {
+        enum MacroType
+        {
+            Type1,
+            Type2,
+            Type3
+        };
+
+        // Skal lige se om jeg kan klare mig uden interface.
+        /*
+        public interface IMacroData
+        {
+            int id { get; }
+            char key { get; set; }
+            string description { get; set; }
+            MacroType type { get; set; }
+            string code { get; set; }
+
+        }
+         */
+        public class MacroData //: IMacroData
+        {
+            int id;
+            char key;
+            string desctiption;
+            MacroType type;
+            string code;
+
+            public MacroData(int _id, char _key, string _desctiption,MacroType _type,string _code)
+            {
+                this.id = _id;
+                this.key = _key;
+                this.desctiption = _desctiption;
+                this.type = _type;
+                this.code = _code;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            MacroData macroData = new MacroData(1, 'A', "Pass1", MacroType.Type1, "Dette er mit hemmelige password 1");
+
+        }
+    }
+}
---
+++ PrototypeMacroData/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrototypeMacroData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrototypeMacroData")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("414e9756-6c0f-4239-a284-90c6c6add56a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
